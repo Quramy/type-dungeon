989a9a8e2bb96c2d4070319f4f4772df11b7c7ed
Yosuke Kurami <yosuke.kurami@gmail.com>
2020-03-12 05:07:28 +0900

    feat: add awaited

---
+++ questions/awaited.ts
@@ -0,0 +1,22 @@
+import "type-dungeon";
+
+/**
+ *
+ * @typeQuestion
+ * Edit the right hand side to match the following expected type conditions.
+ *
+ * @difficulty medium
+ *
+ * @replaceTo
+ * ```
+ * type Awaited<T> = any;
+ * ```
+ */
+type Awaited<T> = (T extends PromiseLike<infer X> ? { 0: Awaited<X> } : { 0: T })[0];
+
+type P1 = Promise<string>;
+type P2 = Promise<Promise<string>>;
+
+// expected
+type S1 = Awaited<P1>; // should be string;
+type S2 = Awaited<P2>; // shuuld be string too;
