a58199cde7fb203f85c39f1fa4668a50032c33f2
Yosuke Kurami <yosuke.kurami@gmail.com>
2020-03-12 21:48:05 +0900

    fix random question

--- questions/randomize.ts
+++ questions/randomize.ts
@@ -3,18 +3,15 @@ import "type-dungeon";
 /**
  * @remove
  */
-type Func<A = any, R = any> = (args: A) => R
+type Func<A = any, R = any> = (args: A) => R;
 
 /**
  * @remove
  */
 type Union<V, U extends any[]> = {
-  0: V
-    1: ((...v: U) => any) extends ((head: infer Head, ...tail: infer Tail) => any)
-    ? Union<V | Head, Tail>
-  : never 
-}[U['length'] extends 0 ? 0 : 1]
-
+  0: V;
+  1: ((...v: U) => any) extends ((head: infer Head, ...tail: infer Tail) => any) ? Union<V | Head, Tail> : never;
+}[U['length'] extends 0 ? 0 : 1];
 
 /**
  *
@@ -32,12 +29,17 @@ type Union<V, U extends any[]> = {
  **/
 type Random = <T, U extends any[]>(first: T, ...remnant: U) => Union<T, U>;
 
-declare const random: Random
+declare const random: Random;
 
-const a = { value: 1 } as const
-const b = { value: 2 } as const
-const c = { value: 3 } as const
+const a = { value: 1 } as const;
+const b = { value: 2 } as const;
+const c = { value: 3 } as const;
 
 // Expected:
 // The `random` return type should be assignable this type annotation.
-const value: { value: 1 } | { value: 2 } | { value: 3 } = random(a, b, c);
+
+const valueAB = random(a, b);
+const valueABC = random(a, b, c);
+
+const x: { readonly value: 1 } | { readonly value: 2 } = valueAB;
+const y: { readonly value: 1 } | { readonly value: 2 } | { readonly value: 3 } = valueABC;
