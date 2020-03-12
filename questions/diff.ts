61facedd5110203754063c1b06e5e3b3bb744408
Yosuke Kurami <yosuke.kurami@gmail.com>
2020-03-12 23:50:00 +0900

    add diff type

---
+++ questions/diff.ts
@@ -0,0 +1,51 @@
+import "type-dungeon";
+
+/**
+ * @typeQuestion
+ *
+ * Complete `$Diff` type to match the following condition.
+ * FYI, Flow type has the same name utility type.
+ *
+ * @difficulty medium
+ *
+ * @replaceTo
+ *
+ * ```
+ * type $Diff<A, B> = any;
+ * ```
+ */
+type $Diff<A, B> = { [P in Extract<keyof A, keyof B>]+?: A[P] } & { [P in Exclude<keyof A, keyof B>]-?: A[P] };
+
+type Props = {
+  id: number;
+  title: string;
+  tags: string[];
+};
+
+const defaultProps = {
+  tags: [],
+};
+
+type RequiredProps = $Diff<Props, typeof defaultProps>;
+
+const props1: RequiredProps = {
+  id: 100,
+  title: "my post",
+};
+
+const props2: RequiredProps = {
+  id: 100,
+  title: "my post",
+  tags: ["ts"],
+};
+
+// should be error
+const props3: RequiredProps = {
+  id: 100,
+  tags: [],
+};
+
+// should be error
+const props4: RequiredProps = {
+  title: "my post",
+};
